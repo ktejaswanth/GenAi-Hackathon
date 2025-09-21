268790e3b14e8128a4fc7907e290c2d6b232b6ff
KONDAVETI TEJASWANTH <2300031638@kluniversity.in>
2025-09-21 14:05:56 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "https://genai-hackathon-nayanas-team.onrender.com/api", // backend URL
 });
 
 // Attach token automatically
